6a96780f235e6c77d21838f9a9b7e7829ec6ca6b
Mohammad <mohammad1f2f@gmail.com>
2022-01-10 21:54:16 +0330

    sign HistoryList

--- src/component/HistoryList.js
+++ src/component/HistoryList.js
@@ -2,13 +2,16 @@ import React,{useContext} from 'react';
 import {GlobalContext} from '../context/GlobalState';
 
 export default function HistoryList() {
-  const {transactions}= useContext(GlobalContext)
+  const {transactions}= useContext(GlobalContext);
+  
+  const sign = transactions.amount > 0 ? '-' : '+';
     return (
+
         <>
           <div>
               <h4>Hstory</h4>
             <ul>
-              {transactions.map(transaction=>(<li>{transaction.text} <span>{transaction.amount}</span></li>))}  
+              {transactions.map(transactions=>(<li>{transactions.text} <span>{sign}${Math.abs(transactions.amount)}</span></li>))}  
             </ul>    
           </div>  
         </>
