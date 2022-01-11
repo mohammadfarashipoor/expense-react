daf63a29ef87e1f33bfcd3c95ea644fc9b5e3716
Mohammad <mohammad1f2f@gmail.com>
2022-01-11 17:27:42 +0330

    income & expense

--- src/component/IncomeExpene.js
+++ src/component/IncomeExpene.js
@@ -1,15 +1,22 @@
-import React from 'react'
+import React ,{useContext} from 'react'
+import {GlobalContext} from '../context/GlobalState';
 
 export default function IncomeExpene() {
+    const {transactions}= useContext(GlobalContext);
+    const amount = transactions.map(transaction =>transaction.amount);
+    const income = amount.filter(item => item >0 ).reduce((acc,item) => (acc+=item), 0).toFixed(2);
+    const expense = amount.filter(item => item <0 ).reduce((acc,item) => (acc+=item), 0).toFixed(2);
+
+
     return (
         <div>
             <div>
                 <h4>Income</h4>
-                <p>0.0$</p>
+                <p>{income}$</p>
             </div>
             <div>
                 <h4>Expense</h4>
-                <p>0.0$</p>
+                <p>{expense}$</p>
             </div>
         </div>
     )
